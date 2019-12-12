b83e57fb2f6f1d6266e6f9dcefa80927554076e2
HyperTitian <bchodsdo@mtu.edu>
2019-12-12 11:09:06 -0500

    LOL :)
    Realz, updated Text directory

---
+++ src/RandomString.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RandomString {
+
+    public RandomString(String s) {
+    }
+
+    public static String RandomString(String fileName) throws FileNotFoundException {
+
+        File file = new File(fileName);
+        Scanner scan = new Scanner(file);
+        int fileLength = scan.nextInt();
+        String[] array = new String[fileLength];
+        double position = Math.random() * fileLength;
+
+        for(int i = 0; i < fileLength; i++){
+            array[i] = scan.nextLine();
+        }
+
+        return array[(int)position+1];
+    }
+}
+
