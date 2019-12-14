257f42aa235c75c867019bd659a8dc6e09f4a4a3
DaDarkWizard <46545299+DaDarkWizard@users.noreply.github.com>
2019-12-13 19:25:06 -0500

    Changed weapon event

--- src/CombatHandler/Weapons/WeaponUseEvent.java
+++ src/CombatHandler/Weapons/WeaponUseEvent.java
@@ -6,14 +6,20 @@ import java.util.EventObject;
 
 public class WeaponUseEvent extends EventObject {
     Combatant source;
+    Weapon weapon;
 
-    public WeaponUseEvent(Combatant source) {
+    public WeaponUseEvent(Combatant source, Weapon weapon) {
         super(source);
         this.source = source;
+        this.weapon = weapon;
     }
 
     @Override
     public Combatant getSource() {
         return this.source;
+    }
+
+    public Weapon getWeapon() {
+        return this.weapon;
     }
 }
