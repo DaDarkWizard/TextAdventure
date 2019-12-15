a5b23e010ce66ae552487b3676e337fc67ef9809
910esmith <58742468+910esmith@users.noreply.github.com>
2019-12-14 22:59:47 -0500

    Update StatHandler.java

--- src/CombatHandler/Weapons/StatHandler.java
+++ src/CombatHandler/Weapons/StatHandler.java
@@ -1,12 +1,22 @@
 package CombatHandler.Weapons;
 
+/**
+ * The class handles the stats of the weapons 
+ *
+ * Date Last Modified: 12/14/2019
+ * @author Daniel Masker, Ben Hodsdon, Emma Smith, Joseph Teahen
+ *
+ * CS1131, fall 2019
+ * Lab Section 2
+ */
+
 public class StatHandler {
 
     public enum Stats {
-        brawn,
-        spiffness,
-        moxy,
-        smerts
+        brawn,          //Strength  
+        spiffness,      //Dexterity 
+        moxy,           // Charisma 
+        smerts          // Intellect 
     }
 
     public static Stats findDefensiveStat(Stats stats) {
