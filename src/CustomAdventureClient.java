842d183fe8b312249622463ad5090e700609698e
DaDarkWizard <46545299+DaDarkWizard@users.noreply.github.com>
2019-12-15 22:33:49 -0500

    Finalized

--- src/CustomAdventureClient.java
+++ src/CustomAdventureClient.java
@@ -1,9 +1,6 @@
-package Client;
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
