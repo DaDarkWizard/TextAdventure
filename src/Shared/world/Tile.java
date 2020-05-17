440bd02a1947789f2b52760fc0827325c95b039b
Daniel Masker <madly123iamme@gmail.com>
2020-05-17 15:17:58 -0500

    Made Tile class. This closes issue #31

--- src/Shared/world/Tile.java
+++ src/Shared/world/Tile.java
@@ -1,4 +1,31 @@
 package Shared.world;
 
+import Shared.util.Vector;
+
+@SuppressWarnings("unused")
+
 public class Tile {
+    protected Vector location;
+    protected String id;
+
+    public Tile(Vector location, String id) {
+        this.location = location;
+        this.id = id;
+    }
+
+    public Vector getLocation() {
+        return location;
+    }
+
+    public void setLocation(Vector location) {
+        this.location = location;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
